0c577ee6a6bc93971824b94f5b14d8ae4ab82b64
Vinayak Sharma <vinayak252@gmail.com>
2021-02-06 20:10:22 +0530

    Basic FnB ends without DB

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Connect Database
+// connectDB();
+
+app.use(express.json({ extended: false }));
+// app.use(express.static(path.join(__dirname, "client/build")));
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => { console.log(`Express Server started at port: ${PORT}`) });
